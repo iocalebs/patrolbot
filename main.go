fbe42de99acf0c6b45ab655892bb1171611a279f
Caleb St-Denis <iocalebst@gmail.com>
2025-12-21 08:50:49 -0500

    Add TODO for integration tests

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 // PatrolBot assists with patrolling tasks on MediaWiki sites.
 package main
 
+// TODO: integration tests
+
 import "github.com/iocalebs/patrolbot/internal/cli"
 
 //go:generate go run internal/tools/genschema/genschema.go
