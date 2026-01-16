e6a16f3d4dcf71e8fa57ab6336c4cb4a5db369ef
Caleb St-Denis <iocalebst@gmail.com>
2026-01-16 11:25:01 -0500

    Add build workflow

--- main.tf
+++ main.tf
@@ -18,3 +18,14 @@ resource "github_repository" "patrolbot" {
   has_issues = true
   has_discussions = true
 }
+
+variable "codecov_token" {
+  type = string
+  sensitive = true
+}
+
+resource "github_actions_secret" "codecov_token" {
+  repository = github_repository.patrolbot.name
+  secret_name = "CODECOV_TOKEN"
+  plaintext_value = var.codecov_token
+}
