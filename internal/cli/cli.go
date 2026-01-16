944927bf93c7098842f83b3b7bb5d54de4fa6d5e
Caleb St-Denis <iocalebst@gmail.com>
2026-01-15 20:37:08 -0500

    Move fang error handler to separate file

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -3,20 +3,12 @@ package cli
 
 import (
 	"context"
-	"errors"
-	"fmt"
-	"io"
 	"os"
-	"strings"
 
-	"charm.land/lipgloss/v2"
 	"github.com/charmbracelet/fang"
-	"github.com/charmbracelet/x/term"
-	"github.com/iocalebs/patrolbot/internal/cli/clierr"
 	configCmd "github.com/iocalebs/patrolbot/internal/cli/commands/config"
 	initCmd "github.com/iocalebs/patrolbot/internal/cli/commands/init"
 	reportCmd "github.com/iocalebs/patrolbot/internal/cli/commands/report"
-	reporter "github.com/iocalebs/patrolbot/internal/report"
 	"github.com/spf13/cobra"
 )
 
@@ -47,75 +39,4 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-// A modification of [github.com/charmbracelet/fang.DefaultErrorHandler] that displays usage help
-// when the command error is of type [UsageError].
-// Fang's default error handler only looks for certain cobra-generated strings.
-// See: https://github.com/spf13/cobra/pull/2266
-func errHandler(w io.Writer, styles fang.Styles, err error) {
-	if w, ok := w.(term.File); ok {
-		// if stderr is not a tty, simply print the error without any
-		// styling or going through an [ErrorHandler]:
-		if !term.IsTerminal(w.Fd()) {
-			_, _ = fmt.Fprintln(w, err.Error())
-			return
-		}
-	}
-
-	_, _ = fmt.Fprintln(w, styles.ErrorHeader.String())
-
-	var multiError *clierr.MultiError
-	if errors.As(err, &multiError) {
-		style := styles.ErrorText.UnsetTransform().Transform(func(s string) string {
-			if s == "" {
-				return s
-			}
-
-			return strings.ToUpper(s[0:1]) + s[1:]
-		})
-		_, _ = fmt.Fprintln(w, style.Render(err.Error()))
-	} else {
-		_, _ = fmt.Fprintln(w, styles.ErrorText.Render(err.Error()+"."))
-	}
-
-	_, _ = fmt.Fprintln(w)
-	if errors.Is(err, reporter.ErrReportTypeNotFound) {
-		_, _ = fmt.Fprintln(w, lipgloss.JoinHorizontal(
-			lipgloss.Left,
-			styles.ErrorText.UnsetWidth().Render("Use"),
-			styles.Program.Flag.Render(" --list "),
-			styles.ErrorText.UnsetWidth().UnsetMargins().UnsetTransform().Render("to see available report types."),
-		))
-		_, _ = fmt.Fprintln(w)
-	} else if isUsageError(err) {
-		_, _ = fmt.Fprintln(w, lipgloss.JoinHorizontal(
-			lipgloss.Left,
-			styles.ErrorText.UnsetWidth().Render("Try"),
-			styles.Program.Flag.Render(" --help "),
-			styles.ErrorText.UnsetWidth().UnsetMargins().UnsetTransform().Render("for usage."),
-		))
-		_, _ = fmt.Fprintln(w)
-	}
-}
-
-// Copied from https://github.com/charmbracelet/fang/blob/v0.4.4/fang.go
-// with slight modification for custom error types.
-func isUsageError(err error) bool {
-	msg := err.Error()
-
-	for _, prefix := range []string{
-		"flag needs an argument:",
-		"unknown flag:",
-		"unknown shorthand flag:",
-		"unknown command",
-		"invalid argument",
-	} {
-		var errUsage clierr.UsageError
-		if strings.HasPrefix(msg, prefix) || errors.As(err, &errUsage) {
-			return true
-		}
-	}
-
-	return false
 }
---
+++ internal/cli/errhandler.go
@@ -0,0 +1,85 @@
+package cli
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+
+	"charm.land/lipgloss/v2"
+	"github.com/charmbracelet/fang"
+	"github.com/charmbracelet/x/term"
+	"github.com/iocalebs/patrolbot/internal/cli/clierr"
+	reporter "github.com/iocalebs/patrolbot/internal/report"
+)
+
+// A modification of [github.com/charmbracelet/fang.DefaultErrorHandler] that displays usage help
+// when the command error is of type [UsageError].
+// Fang's default error handler only looks for certain cobra-generated strings.
+// See: https://github.com/spf13/cobra/pull/2266
+func errHandler(w io.Writer, styles fang.Styles, err error) {
+	if w, ok := w.(term.File); ok {
+		// if stderr is not a tty, simply print the error without any
+		// styling or going through an [ErrorHandler]:
+		if !term.IsTerminal(w.Fd()) {
+			_, _ = fmt.Fprintln(w, err.Error())
+			return
+		}
+	}
+
+	_, _ = fmt.Fprintln(w, styles.ErrorHeader.String())
+
+	var multiError *clierr.MultiError
+	if errors.As(err, &multiError) {
+		style := styles.ErrorText.UnsetTransform().Transform(func(s string) string {
+			if s == "" {
+				return s
+			}
+
+			return strings.ToUpper(s[0:1]) + s[1:]
+		})
+		_, _ = fmt.Fprintln(w, style.Render(err.Error()))
+	} else {
+		_, _ = fmt.Fprintln(w, styles.ErrorText.Render(err.Error()+"."))
+	}
+
+	_, _ = fmt.Fprintln(w)
+	if errors.Is(err, reporter.ErrReportTypeNotFound) {
+		_, _ = fmt.Fprintln(w, lipgloss.JoinHorizontal(
+			lipgloss.Left,
+			styles.ErrorText.UnsetWidth().Render("Use"),
+			styles.Program.Flag.Render(" --list "),
+			styles.ErrorText.UnsetWidth().UnsetMargins().UnsetTransform().Render("to see available report types."),
+		))
+		_, _ = fmt.Fprintln(w)
+	} else if isUsageError(err) {
+		_, _ = fmt.Fprintln(w, lipgloss.JoinHorizontal(
+			lipgloss.Left,
+			styles.ErrorText.UnsetWidth().Render("Try"),
+			styles.Program.Flag.Render(" --help "),
+			styles.ErrorText.UnsetWidth().UnsetMargins().UnsetTransform().Render("for usage."),
+		))
+		_, _ = fmt.Fprintln(w)
+	}
+}
+
+// Copied from https://github.com/charmbracelet/fang/blob/v0.4.4/fang.go
+// with slight modification for custom error types.
+func isUsageError(err error) bool {
+	msg := err.Error()
+
+	for _, prefix := range []string{
+		"flag needs an argument:",
+		"unknown flag:",
+		"unknown shorthand flag:",
+		"unknown command",
+		"invalid argument",
+	} {
+		var errUsage clierr.UsageError
+		if strings.HasPrefix(msg, prefix) || errors.As(err, &errUsage) {
+			return true
+		}
+	}
+
+	return false
+}
