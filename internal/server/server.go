fd59123bc408186d84dd7ed0fa686c1e95b3de0a
Caleb St-Denis <iocalebst@gmail.com>
2026-01-31 11:50:07 -0500

    Add graceful shutdown so that coverage metrics are properly flushed

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,10 +3,14 @@ package server
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 
 	"github.com/iocalebs/patrolbot/internal/config"
 	"github.com/iocalebs/patrolbot/internal/errdefs"
@@ -53,10 +57,30 @@ func NewServer(cfg config.Config, logger *slog.Logger, middleware ...func(http.H
 func (s *Server) Start(ctx context.Context) error {
 	s.logger.InfoContext(ctx, "Starting HTTP server", "addr", s.server.Addr)
 
-	err := s.server.ListenAndServe()
-	if err != nil {
-		return fmt.Errorf("error starting server: %w", err)
+	errChan := make(chan error, 1)
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		err := s.server.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			errChan <- fmt.Errorf("error starting server: %w", err)
+		}
+	}()
+
+	select {
+	case err := <-errChan:
+		return err // Exit early if the server fails to start
+	case <-shutdown:
+		s.logger.InfoContext(ctx, "Initiating graceful shutdown")
 	}
+
+	err := s.server.Shutdown(ctx)
+	if err != nil {
+		s.logger.ErrorContext(ctx, "Graceful shutdown failed")
+	}
+
+	s.logger.InfoContext(ctx, "Graceful shutdown completed")
 
 	return nil
 }
